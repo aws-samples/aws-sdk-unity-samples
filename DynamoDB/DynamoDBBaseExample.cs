771695ad8c598a2780c752a97f5ffd55d2fde574
Karthik Saligrama <karthiksaligrama@users.noreply.github.com>
2015-05-15 15:39:38 -0700

    Update DynamoDBBaseExample.cs

--- DynamoDB/DynamoDBBaseExample.cs
+++ DynamoDB/DynamoDBBaseExample.cs
@@ -1,13 +1,16 @@
-/*
- * Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- *
- * Licensed under the AWS Mobile SDK for Unity Developer Preview License Agreement (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located in the "license" file accompanying this file.
- * See the License for the specific language governing permissions and limitations under the License.
- *
- */
+//
+// Copyright 2014-2015 Amazon.com, 
+// Inc. or its affiliates. All Rights Reserved.
+// 
+// Licensed under the Amazon Software License (the "License"). 
+// You may not use this file except in compliance with the 
+// License. A copy of the License is located 
+// in the "license" file accompanying this file. This file is 
+// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, express or implied. See the License 
+// for the specific language governing permissions and 
+// limitations under the License.
+//
 using UnityEngine;
 using System.Collections;
 using Amazon.DynamoDBv2;
